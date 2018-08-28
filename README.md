3400b58a2a94123a43e6427c557d3400541d2365
William John Holden <wjholden@gmail.com>
2018-08-28 14:11:42 +0900

    Changed input format to tab-separated values.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Node-Monitor
+
+This program is a basic network monitoring tool. It is inferior to robust commercial and open-source solutions like Nagios, Orion, and SNMPc. Why would I bother writing something like this? Because Node-Monitor is really, really easy to configure. There are no annoying wizards to click through. There are no tiny icons to arrange. You just input a tab-separated list of hostnames/IPs, timeouts, intervals, and descriptions as lines of standard input.
+
+## Input
+The input to this program might look like this. The first column is the hostname or IP address of the device. The second column is the timeout, in seconds, that the system waits until considering a ping failed. The third column is the frequency, in seconds, with which we poll the node. It doesn't make much sense to have a timeout greater than or equal to the frequency. The fourth column is a free-form string that will be used to describe the polled device.
+
+```
+198.51.100.1	2	10	My default gateway
+203.0.113.53	2	10	Our DNS server
+192.0.2.25	5	30	The SNMP server
+```
+
+Microsoft Excel can export a spreadsheet as a tab-delimited `*.txt` file that is compatible with this program. Do not include headers. The parser will skip lines containing hostnames deemed invalid by the operating system.
+
+## Usage
+Start the program by piping the input file as standard input. In PowerShell this looks like `Get-Content input.txt | java -jar .\Node_Monitor.jar`.
+
+Press `q` to quit the program. Press `f` to toggle fullscreen. Press `+` and `-` to resize the text and arrow keys to position the text. Nodes are sorted by IP address before the GUI gets constructed.
--- src/com/wjholden/nodemonitor/NodeMonitor.java
+++ src/com/wjholden/nodemonitor/NodeMonitor.java
@@ -4,43 +4,29 @@ import java.net.UnknownHostException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.function.Consumer;
 
 /**
  *
  * @author William John Holden (wjholden@gmail.com)
  */
 public class NodeMonitor {
-    
-    static final List<Node> nodes = new ArrayList<>();
-    static final Map<String, Consumer<String>> parse = new HashMap<>();
-    static int pollInterval = 10;
-    static int pollTimeout = 2;
-    static {
-        parse.put("interval", s -> pollInterval = Integer.valueOf(s));
-        parse.put("timeout", s -> pollTimeout = Integer.valueOf(s));
-        parse.put("node", s -> {
-            s = s.trim();
-            try {
-                if (s.contains(" ")) {
-                    nodes.add(new Node(s.substring(0, s.indexOf(" ")), s.substring(s.indexOf(" ") + 1), pollInterval, pollTimeout));
-                } else {
-                    nodes.add(new Node(s, null, pollInterval, pollTimeout));
-                }
-            } catch (UnknownHostException ex) {
-                System.err.println(ex);
-                System.exit(1);
-            }
-        });
-    }
 
     public static void main(String[] args) throws UnknownHostException {
+        final List<Node> nodes = new ArrayList<>();
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNextLine()) {
-                parse.get(scanner.next()).accept(scanner.nextLine());
+                String[] line = scanner.nextLine().split("\\t");
+                String ip = line[0].trim();
+                int interval = Integer.parseInt(line[1]);
+                int timeout = Integer.parseInt(line[2]);
+                String description = line[3];
+                try {
+                    Node n = new Node(ip, description, interval, timeout);
+                    nodes.add(n);
+                } catch (UnknownHostException e) {
+                    System.err.printf("Unable to add %s. This probably indicates that this is an illegal hostname.%n", ip);
+                }
             }
         }
         Collections.sort(nodes);
