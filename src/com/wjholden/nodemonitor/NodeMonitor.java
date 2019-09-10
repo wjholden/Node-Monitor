a04b23554a55360e7304a65ac904097be8f599f2
William John Holden <wjholden@gmail.com>
2019-09-10 13:32:29 +0900

    Allow input from file instead of stdin

--- src/com/wjholden/nodemonitor/NodeMonitor.java
+++ src/com/wjholden/nodemonitor/NodeMonitor.java
@@ -1,5 +1,9 @@
 package com.wjholden.nodemonitor;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.net.UnknownHostException;
 import java.util.List;
 import java.util.ArrayList;
@@ -12,15 +16,17 @@ import java.util.Scanner;
  */
 public class NodeMonitor {
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws UnknownHostException, FileNotFoundException {
         final List<Node> nodes = new ArrayList<>();
-        try (Scanner scanner = new Scanner(System.in)) {
+        final InputStream input = (args.length == 0) ? System.in : new FileInputStream(new File(args[0]));
+        
+        try (Scanner scanner = new Scanner(input)) {
             while (scanner.hasNextLine()) {
-                String[] line = scanner.nextLine().split("\\t");
-                String ip = line[0].trim();
+                String[] line = scanner.nextLine().split("\\t"); // just a single tab to allow spaces in node description.
+                String ip = line[0].trim(); // apparently there is a weird bug with PowerShell's Get-Content command.
                 int interval = Integer.parseInt(line[1]);
                 int timeout = Integer.parseInt(line[2]);
-                String description = line[3];
+                String description = line.length > 3 ? line[3] : "";
                 try {
                     Node n = new Node(ip, description, interval, timeout);
                     nodes.add(n);
